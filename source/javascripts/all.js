e87a632f2328be48078ebffa20e8e9401be65c57
Samuel Rodriguez <samuelyret@gmail.com>
2017-01-29 19:12:20 +0100

    second deployment

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,12 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+//dropdown menu
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
